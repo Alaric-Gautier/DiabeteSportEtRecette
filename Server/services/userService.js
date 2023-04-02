0d29bab507565058abed217557fb915e7bfbaaea
Yannick G <yann001@hotmail.com>
2023-04-02 11:26:26 +0200

    fix bug bcrypt

--- Server/services/userService.js
+++ Server/services/userService.js
@@ -1,6 +1,6 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcrypt");
 
 //! Doute que data.confirmPassword fonctionne, à tester
 //! Peut-être faire const {data, confirmPassword} = req.body; pour que ça fonctionne
@@ -11,26 +11,26 @@ const userService = {
             const data = req.body;
             // check if all fields are filled
             if (!data) {
-                throw new Error('Please fill all fields');
+                throw new Error("Please fill all fields");
             }
             // if email is already use, throw an error
             if (await prisma.account.findUnique({ where: { email: data.email } })) {
-                throw new Error('Email already exists');
+                throw new Error("Email already exists");
             }
             // check if email is valid
             const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
             if (!emailRegex.test(data.email)) {
-                throw new Error('Email is not valid');
+                throw new Error("Email is not valid");
             }
             // check if password has at least 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 special character
             const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
             if (!passwordRegex.test(data.password)) {
                 // TODO : add a message to explain why the password is not valid
-                throw new Error('Password is not valid');
+                throw new Error("Password is not valid");
             }
             // check if passwords match
             if (data.password !== data.confirmPassword) {
-                throw new Error('Passwords do not match');
+                throw new Error("Passwords do not match");
             }
             // hash password
             const hashedPassword = await bcrypt.hash(data.password, 10);
