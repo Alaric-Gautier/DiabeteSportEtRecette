0ecfab5f5806615ab62cac3296d08c852883155c
Yannick G <yann001@hotmail.com>
2023-04-09 13:27:26 +0200

    create the connect route, controller, service + add middleware to userRoute

--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const user = require("../controllers/userController");
+const { verifyAccessToken } = require("../middlewares/authenticate");
 
 //public
 router.post("/register", user.register);
 
 //protected
-router.get("/user/:id", user.getUserById);
+router.get("/user/:id", verifyAccessToken, user.getUserById);
 
 module.exports = router;
