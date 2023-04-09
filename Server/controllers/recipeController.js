2ac0e62926cc993a376b4c5928eb7b04480a4ba1
Yannick G <yann001@hotmail.com>
2023-04-10 00:40:33 +0200

    modified controller and service to be more close to MVC pattern

--- Server/controllers/recipeController.js
+++ Server/controllers/recipeController.js
@@ -1,8 +1,19 @@
-const userRecipe = require('../services/recipeService');
+const recipeService = require("../services/recipeService");
+const userRecipe = require("../services/recipeService");
 
 const recipeController = {
-    create: (req, res) => {
-        userRecipe.create(req, res);
+    create: async (req, res) => {
+        try {
+            // Get all fields from request body
+            const { title, description, tag, glycemic_charge, duration, difficulty, images } = req.body;
+
+            // Call the service to create a recipe
+            const recipe = await recipeService.create({ title, description, tag, glycemic_charge, duration, difficulty, images });
+
+            res.status(201).json(recipe);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
     },
 };
 
