5c8300da721ffdf803caf550b779db90e9221872
Yannick-Graffi <107129661+Yannick-Graffi@users.noreply.github.com>
2023-04-10 10:04:51 +0200

    Revert "Restruct mvc"

--- Server/routes/connectRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const connect = require("../controllers/connectController");
-const { verifyAccessToken } = require("../middlewares/authenticate");
-
-router.post("/login", connect.login);
-router.get("/logout", verifyAccessToken, connect.logout);
-
-module.exports = router;
