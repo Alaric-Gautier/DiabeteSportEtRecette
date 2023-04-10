4b4f0053b5cd0b59d787c4bbd3b907836eaf5c76
Yannick G <yann001@hotmail.com>
2023-04-10 10:41:01 +0200

    fix errors when merging restruct_mvc

--- Server/routes/connectRoute.js
+++ Server/routes/connectRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const connect = require("../controllers/connectController");
+const { verifyAccessToken } = require("../middlewares/authenticate");
 
 router.post("/login", connect.login);
-router.get("/logout", connect.logout);
+router.get("/logout", verifyAccessToken, connect.logout);
 
 module.exports = router;
