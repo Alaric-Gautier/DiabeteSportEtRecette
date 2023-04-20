9c02ae5623167fc56733aee781114289713ec17b
Alaric-Gautier <92737965+Alaric-Gautier@users.noreply.github.com>
2023-04-20 20:27:25 +0200

    add xls for glucide

--- Server/.config/importIngredients.js
+++ Server/.config/importIngredients.js
@@ -12,25 +12,24 @@ if (connection) {
     console.log("Connected to MySQL database !");
 };
 
-// const jsonData = fs.readFile('./data/ingredients.json', 'utf8', (err, data) => {
-//     if (err) {
-//         console.error(err)
-//         return;
-//     }
-//     console.log(jsonData);
+const jsonData = fs.readFile('./data/ingredients.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err)
+        return;
+    }
+    console.log(jsonData);
 
-//     const ingredients = JSON.parse(data);
+    const ingredients = JSON.parse(data);
 
-//     ingredients.forEach(ingredient => {
-//         // create and increment id
+    ingredients.forEach(ingredient => {
+        // create and increment id
         
-//         const sql = `INSERT INTO ingredient (name, glycemic_index) VALUES ('${ingredient.name}', '${ingredient.glycemic_index}' )`;
-//         connection.query(sql, function (err, result) {
-//             if (err) throw err;
-//             console.log("ingredients inserted into database !");
-//         });
-//     });
+        const sql = `INSERT INTO ingredient (name, glycemic_index) VALUES ('${ingredient.name}', '${ingredient.glycemic_index}' )`;
+        connection.query(sql, function (err, result) {
+            if (err) throw err;
+        });
+    });
 
     connection.end();
-// });
+});
 
