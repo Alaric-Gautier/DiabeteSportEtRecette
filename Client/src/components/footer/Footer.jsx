1cc23246a9b05ad39264437608aa1f4dd0656cb8
Alaric Gautier <alaric.gautier@mytvchain.com>
2023-05-10 16:53:34 +0200

    responsive footer style in progress + add footer icons

--- Client/src/components/footer/Footer.jsx
+++ Client/src/components/footer/Footer.jsx
@@ -3,55 +3,71 @@ import { NavLink } from "react-router-dom";
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="container">
+
+            <div className="footer-container">
+
                 <div className="footer-logo">
                     <NavLink to="/" className="footer-logo" onClick={() => window.scrollTo(0, 0)}>
                         <img src="/images/logos/logo-horizontal-black.png" alt="logo-color" width="300" />
                     </NavLink>
                 </div>
-                <div className="footer-content">
-                    <p className="footer-content-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-                    </p>
-                    <div className="footer-content-social">
-                        <section className="section">
-                            <h3 className="section-title">Social</h3>
-                            <ul className="section-list">
-                                <li className="list-item">
-                                    <NavLink to="/" className="item-link">
-                                        <img src="/images/icons/linkedin.svg" alt="linkedin" width="30" />
-                                        <span className="link-text">LinkedIn 1</span>
-                                    </NavLink>
-                                </li>
-                                <li className="list-item">
-                                    <NavLink to="/" className="item-link">
-                                        <img src="/images/icons/linkedin.svg" alt="linkedin" width="30" />
-                                        <span className="link-text">LinkedIn 2</span>
-                                    </NavLink>
-                                </li>
-                                <li className="list-item">
-                                    <NavLink to="/" className="item-link">
-                                        <img src="/images/icons/github.svg" alt="github" width="30" />
-                                        <span className="link-text">GitHub 1</span>
-                                    </NavLink>
-                                </li>
-                                <li className="list-item">
-                                    <NavLink to="/" className="item-link">
-                                        <img src="/images/icons/github.svg" alt="github" width="30" />
-                                        <span className="link-text">GitHub 2</span>
-                                    </NavLink>
-                                </li>
-                                <li className="list-item">
-                                    <NavLink to="/" className="item-link">
-                                        <img src="/images/icons/malt.svg" alt="malt" width="30" />
-                                        <span className="link-text">Malt 1</span>
-                                    </NavLink>
-                                </li>
-                            </ul>
-                        </section>
+
+                {/* //TODO: Add link to linkedin and malt profiles */}
+
+                <div className="social-container">
+
+                    <div className="social-item">
+                        <NavLink to="/" className="social-link">
+                            <img src="/images/social-icons/linkedin.svg" alt="linkedin" width="30" />
+                            <span>LinkedIn 1</span>
+                        </NavLink>
                     </div>
+
+                    <div className="social-item">
+                        <NavLink to="/" className="social-link">
+                            <img src="/images/social-icons/linkedin.svg" alt="linkedin" width="30" />
+                            <span>LinkedIn 2</span>
+                        </NavLink>
+                    </div>
+
+                    <div className="social-item">
+                        <NavLink to="/" className="social-link">
+                            <img src="/images/social-icons/linkedin.svg" alt="linkedin" width="30" />
+                            <span>LinkedIn 3</span>
+                        </NavLink>
+                    </div>
+
                 </div>
+
+                <div className="professional-container">
+
+                    <div className="professional-item">
+                        <NavLink to="/" className="professional-link">
+                            <img src="/images/professional-icons/malt.svg" alt="malt" width="30" />
+                            <span>Profil Malt</span>
+                        </NavLink>
+                    </div>
+
+                </div>
+
+                <div className="legal-container">
+
+                    <div className="legal-item">
+                        <NavLink to="/legal" className="legal-link">
+                            <span>Mentions légales</span>
+                        </NavLink>
+                    </div>
+
+                    <div className="legal-item">
+                        <NavLink to="/privacy" className="legal-link">
+                            <span>Politique de confidentialité</span>
+                        </NavLink>
+                    </div>
+
+                </div>
+
             </div>
+
         </footer>
     );
 };
