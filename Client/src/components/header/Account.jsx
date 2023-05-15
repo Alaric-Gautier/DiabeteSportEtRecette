85e489ed16b7081e5958fd830e0883f5370a5052
Alaric Gautier <alaric.gautier@hotmail.fr>
2023-05-15 10:41:35 +0200

    add function for close burger menu

--- Client/src/components/header/Account.jsx
+++ Client/src/components/header/Account.jsx
@@ -1,45 +1,46 @@
 import React, { useState, useContext, Fragment } from "react";
 import { useMediaQuery } from "react-responsive";
-import { NavLink } from "react-router-dom";
+import { NavLink, redirect } from "react-router-dom";
 import { AuthContext } from "../../utils/context";
 
-const Account = ({ opened }) => {
+const Account = ({ opened, removeOpenedClass }) => {
     const isMobile = useMediaQuery({ query: "(max-width: 576px)" });
-    const {isAuth, setIsAuth} = useContext(AuthContext)
+    const { isAuth, setIsAuth } = useContext(AuthContext);
 
     const handleLogout = async () => {
-    await factorizedFetch("GET", "logout", {}, true)
-        setIsAuth(false)
+        await factorizedFetch("GET", "logout", {}, true)
+        setIsAuth(false);
+        redirect("/");
     }
 
     return (
         <div className={`account-container ${isMobile ? "mobile" : ""} ${opened ? "opened" : ""} `}>
-            {!isAuth 
-            ? <Fragment>
-                <div className="account-item">
-                    <NavLink to="/login" className="account-link">
-                        <span className="account-link-text">Se connecter</span>
-                    </NavLink>
-                </div>
-                <div className="account-item">
-                    <NavLink to="/register" className="account-link">
-                        <span className="account-link-text">S'inscrire</span>
-                    </NavLink>
-                </div>
-           </Fragment>
-           : <Fragment>
-                <div className="account-item">
-                    <NavLink to="/dashboard" className="account-link">
-                        <span className="account-link-text">Tableau de bord</span>
-                    </NavLink>
-                </div>
+            {!isAuth
+                ? <Fragment>
+                    <div className="account-item">
+                        <NavLink to="/login" className="account-link" onClick={removeOpenedClass}>
+                            <span className="account-link-text">Se connecter</span>
+                        </NavLink>
+                    </div>
+                    <div className="account-item">
+                        <NavLink to="/register" className="account-link" onClick={removeOpenedClass}>
+                            <span className="account-link-text">S'inscrire</span>
+                        </NavLink>
+                    </div>
+                </Fragment>
+                : <Fragment>
+                    <div className="account-item">
+                        <NavLink to="/dashboard" className="account-link" onClick={removeOpenedClass}>
+                            <span className="account-link-text">Tableau de bord</span>
+                        </NavLink>
+                    </div>
 
-                <div className="account-item">
-                    <NavLink to="/logout" className="account-link" onClick={handleLogout}>
-                        <span className="account-link-text">Se déconnecter</span>
-                    </NavLink>
-                </div>
-            </Fragment>
+                    <div className="account-item">
+                        <NavLink to="/logout" className="account-link" onClick={handleLogout}>
+                            <span className="account-link-text">Se déconnecter</span>
+                        </NavLink>
+                    </div>
+                </Fragment>
             }
         </div>
     );
