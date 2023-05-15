8cde9dead5e6ee5d418bc3425af36cc24b285d56
Yannick <yann001@hotmail.com>
2023-05-15 10:48:49 +0200

    confirmUser sendNewLink

--- Client/src/components/pages/login/ConfirmUser.jsx
+++ Client/src/components/pages/login/ConfirmUser.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react"
 import factorizedFetch from "../../../utils/factorizedFetch"
-import { useParams } from "react-router-dom"
+import { NavLink, useParams } from "react-router-dom"
+import { CreateInput } from "../../forms/formComponents";
 
 const ConfirmUser = () => {
     const confirmationCode = useParams();
     const [isConfirmed, setIsConfirmed] = useState(false)
+    const [formData, setFormData] = useState({email:""})
 
     const sendConfirmationCode = async () => {
         const result = await factorizedFetch("GET", `confirmUser/${confirmationCode}`)
@@ -15,6 +17,11 @@ const ConfirmUser = () => {
         }
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await factorizedFetch("POST", "/getNewConfirmationCode",{email:formData.email})
+    }
+
     useEffect(()=>{
         sendConfirmationCode()
     },[])
@@ -23,7 +30,13 @@ const ConfirmUser = () => {
         <div>
             { isConfirmed 
             ? <p>Votre compte a bien été confirmé</p>
-            : <p>Une erreur est survenue, veuillez réessayer</p>
+            : <div>
+                <p>Une erreur est survenue</p>
+                <form onSubmit={handleSubmit}>
+                    <CreateInput inputName={"email"} label={"Email"} inputType="email" formData={formData} setFormData={setFormData}/>
+                    <button type="submit">Envoyer un nouveau lien</button>
+                </form>
+              </div>
             }
         </div>
     )
