0979094ea570476c44d2164f38401ea5916f00d2
Alaric-Gautier <92737965+Alaric-Gautier@users.noreply.github.com>
2023-05-16 23:13:02 +0200

    add context in router

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { Fragment } from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import React, { Fragment, useContext } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./components/pages/MainLayout";
 import Home from "./components/pages/Home";
 import Recipe from "./components/pages/Recipe";
@@ -9,8 +9,11 @@ import MyRecipe from "./components/pages/account/dashboard/pages/Myrecipe";
 import MySportExercise from "./components/pages/account/dashboard/pages/MySportExercise";
 import AuthForm from "./components/forms/AuthForm";
 import ConfirmUser from "./components/pages/login/ConfirmUser";
+import { AuthContext } from "./utils/context";
 
 const App = () => {
+    const { isAuth } = useContext(AuthContext);
+
     return (
         <BrowserRouter>
 
@@ -23,12 +26,12 @@ const App = () => {
                         <Route path="recipe" element={<Recipe />} />
 
                         {/* Auth Route */}
-                        <Route path="/auth/:type" element={<AuthForm/>}/>
-                        <Route path="/confirmUser/:confirmationCode" element={<ConfirmUser/>}/>
+                        <Route path="/auth/:type" element={<AuthForm />} />
+                        <Route path="/confirmUser/:confirmationCode" element={<ConfirmUser />} />
 
                         {/* Protected routes */}
                         {/* //TODO Add a protection and redirection */}
-                        <Route path="dashboard/" element={<DashboardLayout />} >
+                        <Route path="dashboard/" element={isAuth ? <DashboardLayout /> : <Navigate to="/auth/login" />}>
                             <Route path="my-account" element={<MyAccount />} />
                             <Route path="my-recipes" element={<MyRecipe />} />
                             <Route path="my-sport-exercises" element={<MySportExercise />} />
