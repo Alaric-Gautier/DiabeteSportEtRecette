cc76950ba7d1a11182da3c8574003b33845575b4
Alaric-Gautier <alaric.gautier@hotmail.fr>
2023-05-17 14:10:29 +0200

    register form design test ok, miss responsive

--- Client/src/components/forms/AuthForm.jsx
+++ Client/src/components/forms/AuthForm.jsx
@@ -1,23 +1,25 @@
-import { useContext, useEffect, useState } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import { useParams, NavLink } from 'react-router-dom';
 import { CreateCheckbox, CreateInput } from './formComponents';
 import { AuthContext } from '../../utils/context';
+import { useMediaQuery } from 'react-responsive';
 
 const AuthForm = () => {
+    const isMobile = useMediaQuery({ query: '(max-width: 576px)' });
     const { type } = useParams();
     const [formError, setFormError] = useState(null);
     const [formType, setFormType] = useState(type);
     const [formData, setFormData] = useState({
-        firstName:"",
-        lastName:"",
-        email:"",
-        birthDate:"",
-        is_diabetic:false,
-        diabetes_type:null,
-        password:"",
-        confirmPassword:""
+        firstName: "",
+        lastName: "",
+        email: "",
+        birthDate: "",
+        is_diabetic: false,
+        diabetes_type: null,
+        password: "",
+        confirmPassword: ""
     });
-    const {login, setIsAuth} = useContext(AuthContext)
+    const { login, setIsAuth } = useContext(AuthContext)
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -49,27 +51,39 @@ const AuthForm = () => {
     useEffect(() => {
         setFormError(null);
         setFormData({
-            firstName:"",
-            lastName:"",
-            email:"",
-            birthDate:"",
-            is_diabetic:false,
-            diabetes_type:null,
-            password:"",
-            confirmPassword:""
-        }); 
+            firstName: "",
+            lastName: "",
+            email: "",
+            birthDate: "",
+            is_diabetic: false,
+            diabetes_type: null,
+            password: "",
+            confirmPassword: ""
+        });
         setFormType(type)
     }, [type])
 
     return (
-        <div className="authForm-container">
+        <div className={`authForm-container ${isMobile ? "mobile" : ""} `}>
+
+            <div className="form-banner">
+                <img src="/images/dashboard/backgrounds/myAccount.jpg" alt="banner" />
+            </div>
+
             <form onSubmit={handleSubmit} className="auth-form">
+
+                <div className="authForm-logo">
+                    <img src="/images/logos/logo-vertical-color.svg" alt="logo" />
+                </div>
+
                 {formError && <div className="error-message">{formError}</div>}
 
                 {formType === "login" && (
                     <>
-                        <CreateInput inputName={"email"} label={"Email"} inputType="email" formData={formData} setFormData={setFormData}/>                   
-                        <CreateInput inputName={"password"} label={"Mot de passe"} inputType="password" formData={formData} setFormData={setFormData}/>
+                        <h1>Connexion</h1>
+
+                        <CreateInput inputName={"email"} label={"Email"} inputType="email" formData={formData} setFormData={setFormData} />
+                        <CreateInput inputName={"password"} label={"Mot de passe"} inputType="password" formData={formData} setFormData={setFormData} />
 
                         <button type="submit">Connexion</button>
 
@@ -83,28 +97,38 @@ const AuthForm = () => {
                 )}
 
                 {formType === "register" && (
-                    
-                    <>
-                        <CreateInput inputName={"lastName"} label="Nom" formData={formData} setFormData={setFormData}/>
-                        <CreateInput inputName={"firstName"} label="Prénom" formData={formData} setFormData={setFormData}/>
-                        <CreateInput inputName={"email"} label="Email" inputType="email" formData={formData} setFormData={setFormData}/>
-                        <CreateInput inputName={"birthDate"} label="Date de naissance" inputType="date" formData={formData} setFormData={setFormData}/>
-                        <CreateCheckbox inputName={"is_diabetic"} label="Êtes-vous diabétique ?" formData={formData} setFormData={setFormData}/>
-                        {formData.is_diabetic && <CreateInput inputName={"diabetes_type"} label="Type de diabète" inputType="email" formData={formData} setFormData={setFormData}/>}
-                        <CreateInput inputName={"password"} label="Mot de passe" inputType="password" formData={formData} setFormData={setFormData}/>
-                        <CreateInput inputName={"confirmPassword"} label="Confirmer le mot de passe" inputType="password" formData={formData} setFormData={setFormData}/>
 
-                        <button type="submit">Sign Up</button>
+                    <Fragment>
 
-                        <p>
-                            Vous avez déjà un compte ? <NavLink to="/auth/login">Connectez-vous ici</NavLink>.
-                        </p>
-                    </>
+                        <div className="form-title">
+                            <h1>Inscription</h1>
+                        </div>
+
+                        <div className="form-fields">
+                            <CreateInput inputName={"lastName"} label="Nom" formData={formData} setFormData={setFormData} />
+                            <CreateInput inputName={"firstName"} label="Prénom" formData={formData} setFormData={setFormData} />
+                            <CreateInput inputName={"email"} label="Email" inputType="email" formData={formData} setFormData={setFormData} />
+                            <CreateInput inputName={"birthDate"} label="Date de naissance" inputType="date" formData={formData} setFormData={setFormData} />
+                            <CreateCheckbox inputName={"is_diabetic"} label="Êtes-vous diabétique ?" formData={formData} setFormData={setFormData} />
+                            {formData.is_diabetic && <CreateInput inputName={"diabetes_type"} label="Type de diabète" inputType="email" formData={formData} setFormData={setFormData} />}
+                            <CreateInput inputName={"password"} label="Mot de passe" inputType="password" formData={formData} setFormData={setFormData} />
+                            <CreateInput inputName={"confirmPassword"} label="Confirmer le mot de passe" inputType="password" formData={formData} setFormData={setFormData} />
+                        </div>
+
+                        <div className="form-submit">
+                            <button type="submit">S'inscrire</button>
+                        </div>
+
+                        <div className="form-footer">
+                            <p>Vous avez déjà un compte ?</p> <NavLink to="/auth/login">Connectez-vous ici.</NavLink>
+                        </div>
+
+                    </Fragment>
                 )}
 
                 {formType === "forgot-password" && (
-                    <>  
-                        <CreateInput inputName={"forgotPassword"} label={"Email"} inputType="email" formData={formData} setFormData={setFormData}/>
+                    <>
+                        <CreateInput inputName={"forgotPassword"} label={"Email"} inputType="email" formData={formData} setFormData={setFormData} />
 
                         <button type="submit">Réinitialiser le mot de passe</button>
                     </>
@@ -112,14 +136,16 @@ const AuthForm = () => {
 
                 {formType === "reset-password" && (
                     <>
-                        <CreateInput inputName={"password"} label="Mot de passe" inputType="password" formData={formData} setFormData={setFormData}/>
-                        <CreateInput inputName={"confirmPassword"} label="Confirmer le mot de passe" inputType="password" formData={formData} setFormData={setFormData}/>
+                        <CreateInput inputName={"password"} label="Mot de passe" inputType="password" formData={formData} setFormData={setFormData} />
+                        <CreateInput inputName={"confirmPassword"} label="Confirmer le mot de passe" inputType="password" formData={formData} setFormData={setFormData} />
 
                         <button type="submit">Confirmer</button>
                     </>
                 )}
+
             </form>
-        </div>
+
+        </div >
     );
 
 }
